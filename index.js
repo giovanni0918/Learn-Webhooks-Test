8beedbddf91fdd1550d90de2eb792ed9ef3846aa
Giovanni Orlando <gorlandor@gmail.com>
2021-03-22 14:49:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+const Crypto = require('crypto');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.body.pages[0].title) {
-        context.res = {
-            body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
-        };
+    const hmac = Crypto.createHmac("sha1", "<default_key>");
+    const signature = hmac.update(JSON.stringify(req.body)).digest('hex');
+    const shaSignature = `sha1=${signature}`;
+    const gitHubSignature = req.headers['x-hub-signature'];
+
+    if (!shaSignature.localeCompare(gitHubSignature)) {
+        if (req.body.pages[0].title) {
+            context.res = {
+                body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
+            };
+        }
+        else {
+            context.res = {
+                status: 400,
+                body: ("Invalid payload for Wiki event")
+            }
+        }
     }
     else {
         context.res = {
-            status: 400,
-            body: ("Invalid payload for Wiki event")
-        }
+            status: 401,
+            body: "Signatures don't match"
+        };
     }
+
 }
